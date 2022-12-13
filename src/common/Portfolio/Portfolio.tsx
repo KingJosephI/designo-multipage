227bb3201956e986fffc8bc41c5fd8484b608f85
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-12-13 12:48:27 +0100

    feat: create porfolio list

---
+++ src/common/Portfolio/Portfolio.tsx
@@ -0,0 +1,23 @@
+import PortfolioCard from '../PortfolioCard/PortfolioCard';
+import './Portfolio.scss';
+
+interface IPortfolio extends React.BaseHTMLAttributes<HTMLDivElement> {
+  data?: Array<any>;
+}
+
+const Portfolio = ({ data, ...props }: IPortfolio) => {
+  return (
+    <section className="portfolio" {...props}>
+      {data?.map(({ title, description, image, id }) => (
+        <PortfolioCard
+          key={id}
+          title={title}
+          description={description}
+          image={image}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/common/index.tsx
+++ src/common/index.tsx
@@ -3,3 +3,4 @@ export { default as Card } from './Card/Card';
 export { default as Intro } from './Intro/Intro';
 export { default as LetsTalkSection } from './LetsTalkSection/LetsTalkSection';
 export { default as ServicesList } from './ServicesList/ServicesList';
+export { default as Portfolio } from './Portfolio/Portfolio';
