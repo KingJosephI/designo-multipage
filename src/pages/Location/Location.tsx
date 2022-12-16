d870d41b53df70276c8367c0936f50782c234712
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-12-16 03:55:16 +0100

    chore: install react-leaflet to insert map in the document

--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,14 +1,68 @@
-import { useEffect } from 'react';
 import { Layout, LetsTalkSection } from '../../common';
 
 const Location = () => {
-  useEffect(() => {
-    // 👇️ scroll to top on page load
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  }, []);
   return (
     <Layout>
-      <h1>Location</h1>
+      <section>
+        <div className="map"></div>
+        <div>
+          <h2>Canada</h2>
+          <div>
+            <p>
+              <strong>Designo Central Office</strong>
+              <address>
+                3886 Wellington Street <br />
+                Toronto, Ontario M9C 3J5 <br />
+              </address>
+            </p>
+            <p>
+              <strong>Contact</strong> <br /> P :
+              <a href="tel:+1 253-863-8967">+1 253-863-8967 </a> <br />M :
+              <a href="http://contact@designo.co">contact@designo.co</a> <br />
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="map"></div>
+        <div>
+          <h2>Australia</h2>
+          <div>
+            <p>
+              <strong>Designo AU Office</strong>
+              <address>
+                19 Balonne Street <br />
+                New South Wales 2443 <br />
+              </address>
+            </p>
+            <p>
+              <strong>Contact</strong> <br /> P :
+              <a href="tel:(02) 6720 9092">(02) 6720 9092</a> <br />M :
+              <a href="http://contact@designo.co">contact@designo.au</a> <br />
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="map"></div>
+        <div>
+          <h2>United Kingdom</h2>
+          <div>
+            <p>
+              <strong>Designo UK Office</strong>
+              <address>
+                13 Colorado Way <br />
+                Rhyd-y-fro SA8 9GA <br />
+              </address>
+            </p>
+            <p>
+              <strong>Contact</strong> <br /> P :
+              <a href="tel:078 3115 1400">078 3115 1400</a> <br />M :
+              <a href="http://contact@designo.uk">contact@designo.uk</a> <br />
+            </p>
+          </div>
+        </div>
+      </section>
       <LetsTalkSection />
     </Layout>
   );
