62f5ce1bdc51e6b373c98285a62bfe7c935c3f20
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-12-18 02:02:17 +0100

    chore: fix the build errors

--- src/pages/LocationPage/MyMap.tsx
+++ src/pages/LocationPage/MyMap.tsx
@@ -7,17 +7,13 @@ function MyMap() {
     'pk.eyJ1IjoiamthYmVtYmEyMDIzIiwiYSI6ImNsYnMxbXJzaDBtMnQzb3FkMGN1N21zMHgifQ.z1h3DFIQvWMqxd4YN_XiKQ';
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef(null);
-  // const [lng, setLng] = useState(-3.6);
-  // const [lat, setLat] = useState(24.7);
-  // const [lng, setLng] = useState(-70.9);
-  // const [lat, setLat] = useState(42);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
+    new mapboxgl.Map({
       container: mapContainer.current || '',
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
